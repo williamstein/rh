bc1a403490b3989f767b140dde57df4c3315f4ef
William Stein <wstein@gmail.com>
2015-04-14 13:37:43 +0000

    added the word "calculus" to the index

--- rh/bibnote_search.py
+++ rh/bibnote_search.py
@@ -4,26 +4,25 @@
 import os, sys
 
 filename='rh.tex'
-search_term = "\\bibnote{"
-search_term = "\\footnote{"
-s = open(filename).read()
 
-i = 0
-while True:
-    i = s.find(search_term, i)
-    if i == -1:
-        break
-    i += len(search_term)
-    i0 = i
-    paren_count = 1
-    while paren_count > 0:
-        if s[i] == '{':
-            paren_count += 1
-        elif s[i] == '}':
-            paren_count -= 1
-        i += 1
-    j = s[i0:i].find("\\index")
-    if j != -1:
-        print '\n' + s[i0+j-50:i0+j+50]+'-'*70+'\n'
+for search_term in ["\\bibnote{", "\\footnote{"]:
+    s = open(filename).read()
+    i = 0
+    while True:
+        i = s.find(search_term, i)
+        if i == -1:
+            break
+        i += len(search_term)
+        i0 = i
+        paren_count = 1
+        while paren_count > 0:
+            if s[i] == '{':
+                paren_count += 1
+            elif s[i] == '}':
+                paren_count -= 1
+            i += 1
+        j = s[i0:i].find("\\index")
+        if j != -1:
+            print '\n' + s[i0+j-50:i0+j+50]+'-'*70+'\n'
 
 
